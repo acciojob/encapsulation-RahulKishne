ea1ba05bdf01b9d61c13f0c8e0bc56f02cb79cf9
Rahul Kishne <kishnerahul123@gmail.com>
2023-08-09 12:56:52 +0530

    Attempt1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,8 +6,8 @@ public class Main {
         // Task 4: Try setting a value to 'name' directly and print it (will result in error)
         // obj.name = "John"; // This will give an error
         // System.out.println(obj.name); // This will also give an error
-        rwOnly.setter("Rahul");
-        String retrieveName=rwOnly.getter();
+        rwOnly.setName("Rahul");
+        String retrieveName=rwOnly.getName();
         System.out.println(retrieveName);
     }
 }
